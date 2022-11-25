e74bfff067a0ad78906ed9cde37620ba8825e515
Alfredo Vicente <alviboi@gmail.com>
2022-11-25 12:34:56 +0100

    a

--- cefire/resources/js/components/DiaComponent.vue
+++ cefire/resources/js/components/DiaComponent.vue
@@ -573,7 +573,7 @@ export default {
             }
         })
         .catch((err) => {
-          this.$toast.error(err.response.data.message);
+          this.$toast.error(err.response.data);
         });
     },
     // Afegix qualsevol altre element que necessite hora a la base de dades
