e5cd001b1d4d15562302fa842023127907f3b810
Alfredo Vicente <alviboi@gmail.com>
2023-01-31 13:32:13 +0100

    cur enlloc de curs

--- cefire/resources/js/components/DiaComponent.vue
+++ cefire/resources/js/components/DiaComponent.vue
@@ -118,7 +118,7 @@
             class="s-curs list-complete-item"
             :key="'cur' + cur.id"
             data-uk-tooltip="pos: right; animation: true; offset: 12;"
-            :title="curs.inici + '-' + curs.fi +' ' + cur.curs"
+            :title="cur.inici + '-' + cur.fi +' ' + cur.curs"
           >
             <span @click="borra_par('curs', cur.id)" class="cerrar" />
           </div>
